9fbed501a3f4ef46010f615a5cc3c56f16ca55ad
Jae423 <45524635+Jae423@users.noreply.github.com>
2019-05-12 22:49:07 -0400

    jj

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,5 @@
-var stepSlider = document.getElementById('myRange');
+//$(".timeline-panel").hide(0);
 
-noUiSlider.create(stepSlider, {
-    start: [4000],
-    step: 1000,
-    range: {
-        'min': [2000],
-        'max': [10000]
-    }
-});
-
-var stepSliderValueElement = document.getElementById('slider-step-value');
-
-stepSlider.noUiSlider.on('update', function (values, handle) {
-    stepSliderValueElement.innerHTML = values[handle];
+$("i").click(function() {
+	    $('.timeline-panel').show(0);
 });
